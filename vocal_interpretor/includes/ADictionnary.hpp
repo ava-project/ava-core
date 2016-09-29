a781b2ee8d659145a71753d9bceaee1e55973f63
Guiz <guillaume.briard@epitech.eu>
2016-09-29 16:25:07 +0200

    Dictionnaries classes basic files

---
+++ vocal_interpretor/includes/ADictionnary.hpp
@@ -0,0 +1,13 @@
+// AVA
+// VOCAL INTERPRETOR
+// ADictionnary.cpp
+
+#ifndef ADICTIONNARY_HPP_
+# define ADICTIONNARY_HPP_
+
+class     ADictionnary {
+public:
+  virtual ~ADictionnary();
+};
+
+#endif // !ADICTIONNARY_HPP_
---
+++ vocal_interpretor/src/CmdDictionnary.cpp
@@ -0,0 +1,15 @@
+// AVA
+// VOCAL INTERPRETOR
+// CmdDictionnary.cpp
+
+#include "CmdDictionnary.hpp"
+
+CmdDictionnary::CmdDictionnary() {}
+
+CmdDictionnary::~CmdDictionnary() {}
+
+bool  CmdDictionnary::loadDictionnary(std::string) {}
+
+bool  CmdDictionnary::unloadDictionnary(std::string) {}
+
+std::vector<std::string>  CmdDictionnary::getCmds() const {}
