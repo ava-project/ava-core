f6baa197a316d06ee2a5da81c12ff2cf129a9149
Dorian Amouroux <dor.amouroux@gmail.com>
2017-01-25 01:28:33 -0800

    endpoint for authentication

--- api-interface/Interface.py
+++ api-interface/Interface.py
@@ -1,13 +1,12 @@
 from flask import Flask
 
+from endpoints import authentication
+
 class InterfaceServer(object):
 
     def __init__(self, **kwargs):
         self.app = Flask(__name__)
-
-        @self.app.route("/")
-        def hello():
-            return "Hello World!"
+        authentication.register_routes(self.app)
 
     def start(self):
-        self.app.run()
+        self.app.run(debug=True)
