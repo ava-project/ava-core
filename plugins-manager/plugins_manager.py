8ef4973442d64b69211000bd7b16ff94aff789f2
Tommy <thomas.milox@epitech.eu>
2017-03-17 03:09:39 +0100

    implemented 'uninstall' function to remove a plugin directory and all its content as well as itself from the dictionary. update 'retrieve_plugins_name_and_files_extension' function, it raises an error if the given path to the plugins directory is invalid. Reduce computations. added some comments.

--- plugins-manager/plugins_manager.py
+++ plugins-manager/plugins_manager.py
@@ -1,10 +1,28 @@
-from utils import retrieve_plugins_name_and_files_extension
+from utils import retrieve_plugins_name_and_files_extension, remove_directory
 
 class plugins_manager(object):
-    "This class handles plugins"
+    "Handles AVA plugins"
+
+    # Constructor
+    # @param: string (/path/to/the/plugins/directory)
     def __init__(self, path):
         self.path = path
         self.plugins_list = {}
 
+    # Run the 'plugins' directory and list all plugins name as well as provided
+    # files' extension.
     def load_plugins(self):
-        retrieve_plugins_name_and_files_extension(self.path, "json", self.plugins_list)
+        try:
+            retrieve_plugins_name_and_files_extension(self.path, "json", self.plugins_list)
+        except:
+            print("[plugins_manager]: invalid path to the plugins' directory.")
+
+    # Uninstall a plugin by removing the plugin's directory and all its content.
+    # @param: string (plugin to uninstall)
+    def uninstall(self, plugin):
+        try:
+            remove_directory(self.path + '/' + plugin)
+        except:
+            print("[plugins_manager]: Specified plugin doesn't exist [" + plugin + "]")
+            return
+        self.plugins_list.pop(plugin, None)
--- plugins-manager/utils.py
+++ plugins-manager/utils.py
@@ -1,8 +1,27 @@
-import os
+import os, shutil
 
+# Handler for retrieving plugins names and files' extension
+# @params:
+#   - path: string  (/path/to/the/plugins/directory)
+#   - skip: string  (extension to skip i.e "json")
+#   - dictionary: the dictionary to fill.
+#
+# @behave: raise an error if the specified directory does not exist.
 def retrieve_plugins_name_and_files_extension(path, skip, dictionary):
+    if os.path.isdir(path) == False:
+        raise
     for directory in os.listdir(path):
         for files in os.listdir(path + '/' + directory):
-            if files[files.find(".") + 1:] == skip:
-                continue
-            dictionary[directory] = files[files.find(".") + 1:]
+            if files.find(".") > 0 and files[files.find(".") + 1:] != skip:
+                dictionary[directory] = files[files.find(".") + 1:]
+
+# Handler for removing a directory and all its content
+# @param:
+#   - path: string (/path/to/the/directory/to/remove)
+#
+# @behave: raise an error if the specified directory does not exist.
+def remove_directory(path):
+    if os.path.isdir(path) == True:
+        shutil.rmtree(path)
+    else:
+        raise
