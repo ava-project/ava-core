4a432ce1e3f4ac204c570ffc5cd4942e97574d7e
Tommy <thomas.milox@epitech.eu>
2017-04-27 23:56:16 +0200

    set up demo

--- plugins-manager/tests.py
+++ plugins-manager/tests.py
@@ -2,36 +2,25 @@ import avasdk
 from plugins_manager import plugins_manager
 
 if __name__ == "__main__":
-    # Print the version of the avasdk
-    print(avasdk.__version__)
-    # Instanciate the plugins manager
-    # param: the path to the plugins' repository
+    #
     manager = plugins_manager("./plugins")
-    # Load the plugins, this method must be called just after the instanciation
-    # of the plugins manager.
     manager.load_plugins()
-    # Install a plugin by specifying the path towards the zipfile containing the plugin.
-    # If an user wants to install a plugin through the CLI, the same method must be used.
-    manager.install("./atom.zip")
-    # Print cached information for a specific plugin.
-    print(manager.plugins_list['atom'])
-    # Uninstall the specified plugin.
-    manager.uninstall("atom")
+
     #
-    # Examples:
-    #
-    # Below some examples of how the 'run' method should be used.
-    (result, status) = manager.run("git", "version")
-    print(result, status)
-    (result, status) = manager.run("ssh", "connect")
-    print(result, status)
-    #
-    # Examples:
-    #
-    # Below some examples of how the 'get_commands' method should be used.
-    test = manager.get_commands("toto")
-    if test is not None:
-        print(test)
+    # manager.install("./demo/ssh.zip")
+
+    print('\n', manager.plugins_list['git'], '\n')
     test = manager.get_commands("git")
     if test is not None:
-        print(test)
+        print('\n', test, '\n')
+
+    #
+    (result, status) = manager.run("git", "version")
+    print('\n', result, status, '\n')
+
+    #
+    (result, status) = manager.run("ssh", "connect")
+    print(result, status, '\n')
+
+    # Uninstall the specified plugin.
+    # manager.uninstall("ssh")
