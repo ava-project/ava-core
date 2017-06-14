c85c740034ffba50dfdb9c072a1df76b10e184db
Tommy <thomas.milox@epitech.eu>
2017-06-14 17:39:24 +0200

    Update plugins using the python model from avasdk. Update manifest.json (added description for each command, tags and build)

--- core/plugins_manager/plugins/git/git.py
+++ core/plugins_manager/plugins/git/git.py
@@ -1,20 +1,20 @@
 import subprocess
+from avasdk.plugins.python_model import PythonModel
 
-class git(object):
-    "Git plugin"
+class git(PythonModel):
+    """
+        Git plugin
+    """
 
-    def __init__(self):
-        self.commands = {
-            'log': self.log,
-            'version': self.version,
-        }
+    def __init__(self, name="Git"):
+        super(git, self).__init__(name)
+        self.set_commands_list({**PythonModel._commands, **{\
+        "log" : self.log, \
+        "version" : self.version, \
+        }})
 
     def log(self, decoy=''):
         subprocess.call(["git", "log"])
 
     def version(self, decoy=''):
         subprocess.call(["git", "version"])
-
-
-    def get_commands(self):
-        return self.commands
