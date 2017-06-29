0b8a6941beba2866fff21e4831323580a3a78948
Tommy <thomas.milox@epitech.eu>
2017-06-29 23:15:11 +0200

    fix mistakes and typo according comments

--- core/vocal_interpretor/interpretor.py
+++ core/vocal_interpretor/interpretor.py
@@ -13,31 +13,30 @@ class Interpretor(object):
         self._thread = threading.Thread(None, self._run)
         Interpretor._daemon = daemon
 
-
     def _run(self):
         """
         """
-        self._should_stop = False
         while not self._should_stop:
             try:
                 inp = input("$> ")
                 if "exit" in inp:
+                    self._should_stop = True
+                    Interpretor._daemon.stop()
                     break
                 Interpretor._daemon.add_event(Event(inp, False, 0))
-
             except KeyboardInterrupt:
+                self._should_stop = True
+                Interpretor._daemon.stop()
                 break
-
             except EOFError:
+                self._should_stop = True
+                Interpretor._daemon.stop()
                 break
-
-        self._should_stop = True
-        Interpretor._daemon.stop()
-        return
 
     def run(self):
         """
         """
+        self._should_stop = False
         self._thread.start()
 
     def stop(self):
